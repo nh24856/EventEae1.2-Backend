b72be728f2ddc9147c66538fe1f9e0602310a820
eazi201 <kfablionel@gmail.com>
2025-04-28 21:16:41 +0200

    Create models

---
+++ EventEae1.2-Backend/Models/Users.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEae1._2_Backend.Models
+{
+   
+        public class User
+        {
+            
+            public string Id { get; set; }
+
+            
+            public string Firstname { get; set; }
+
+           
+            public string Lastname { get; set; }
+
+           
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+
+           
+            [RegularExpression("user|manager|admin")]
+            public string Role { get; set; } = "user";
+
+         
+            public string? Organization { get; set; }
+
+         
+            [RegularExpression("approved|pending|rejected")]
+            public string Status { get; set; } = "approved";
+        }
+    
+}
